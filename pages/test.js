51c34b19fdf3df30bb514c8311389bec99cf278a
AimHigher77 <wnsgus821@gmail.com>
2022-06-10 01:44:05 +0900

    feat(test.js)

--- pages/test.js
+++ pages/test.js
@@ -9,7 +9,9 @@ export default function test() {
       </figure>
       <div className="card-body">
         <h2 className="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <p>
+          If a dowefawfwefwef!!!! haenug chews shoes whose shoes does he choose?
+        </p>
         <div className="card-actions justify-end">
           <button className="btn btn-primary">Buy Now</button>
         </div>
