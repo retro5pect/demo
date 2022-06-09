509c01fc0df63a558591de527a820004bc691009
AimHigher77 <wnsgus821@gmail.com>
2022-06-10 03:10:33 +0900

    fix(test.js : class->className)

--- pages/test.js
+++ pages/test.js
@@ -1,13 +1,13 @@
 export default function test() {
   return (
     <div>
-      <ul class="steps">
-        <li class="step step-primary">1. 퀴즈 설정</li>
-        <li class="step step-primary">2. 문제 설정</li>
-        <li li class="step">
+      <ul className="steps">
+        <li className="step step-primary">1. 퀴즈 설정</li>
+        <li className="step step-primary">2. 문제 설정</li>
+        <li li className="step">
           3. 완료!
         </li>
-        <li class="step">4. 부가 설정</li>
+        <li className="step">4. 부가 설정</li>
       </ul>
     </div>
   );
