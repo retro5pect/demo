77f4a691a1dc17b94a349fee2b4785a455772046
AimHigher77 <wnsgus821@gmail.com>
2022-06-10 14:39:19 +0900

    feat(teach.js)

--- pages/teach.js
+++ pages/teach.js
@@ -10,44 +10,45 @@ export default function create() {
           value="40"
           max="100"
         ></progress>
+        <p>{}</p>
       </div>
 
       <div className="flex justify-between">
-        <div>
-          <div className="card card-compact w-36 bg-base-100 shadow-xl">
-            <figure>
-              <img src="/../public/halla.jpeg" alt="halla" />
-            </figure>
+        <div className="w-40">
+          <div className="card card-compact w-38 bg-base-100 shadow-xl">
+            <figure></figure>
             <div className="card-body">
               <h2 className="card-title">Q1.</h2>
               <p>우리나라에서 가장 고도가 높은 산은?</p>
             </div>
           </div>
 
-          <div className="card card-compact w-36 bg-base-100 shadow-xl">
+          <div className="card card-compact w-38 bg-base-100 bg-gray-300 shadow-xl">
             <figure></figure>
             <div className="card-body">
               <h2 className="card-title">Q2.</h2>
-              <p></p>
+              <p>우리나라에서 장마가 시작되는 계절은?</p>
             </div>
           </div>
 
-          <div className="card card-compact w-36 bg-base-100 shadow-xl">
-            <figure>
-              <img
-                src="https://api.lorem.space/image/shoes?w=400&h=225"
-                alt="Shoes"
-              />
-            </figure>
+          <div className="card card-compact w-38 bg-base-100 shadow-xl">
+            <figure></figure>
+            <div className="card-body">
+              <h2 className="card-title">[Non-sense]</h2>
+              <p>어떤 퀴즈가 나올까요?</p>
+            </div>
+          </div>
+          <div className="card card-compact w-38 bg-base-100 shadow-xl">
+            <figure></figure>
             <div className="card-body">
               <h2 className="card-title">Q3.</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <p>지구계 구성요소에는 생물권이 있다</p>
             </div>
           </div>
         </div>
 
         <div> hi </div>
-        <div className="w-48"></div>
+        <div className="w-40"></div>
       </div>
     </div>
   );
