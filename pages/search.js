f2c83ef68ff7aa945cd1dc10bf5b1dd544637556
AimHigher77 <wnsgus821@gmail.com>
2022-06-10 14:42:00 +0900

    fix(search.js)

---
+++ pages/search.js
@@ -0,0 +1,55 @@
+import { info } from "daisyui/src/colors";
+import Image from "next/image";
+
+export default function create() {
+  return (
+    <div>
+      <div className="my-10 flex justify-center">
+        <progress
+          className="progress progress-info w-56"
+          value="40"
+          max="100"
+        ></progress>
+        <p>{}</p>
+      </div>
+
+      <div className="flex justify-between">
+        <div className="w-40">
+          <div className="card card-compact w-38 bg-base-100 shadow-xl">
+            <figure></figure>
+            <div className="card-body">
+              <h2 className="card-title">Q1.</h2>
+              <p>우리나라에서 가장 고도가 높은 산은?</p>
+            </div>
+          </div>
+
+          <div className="card card-compact w-38 bg-base-100 bg-gray-300 shadow-xl">
+            <figure></figure>
+            <div className="card-body">
+              <h2 className="card-title">Q2.</h2>
+              <p>우리나라에서 장마가 시작되는 계절은?</p>
+            </div>
+          </div>
+
+          <div className="card card-compact w-38 bg-base-100 shadow-xl">
+            <figure></figure>
+            <div className="card-body">
+              <h2 className="card-title">[Non-sense]</h2>
+              <p>어떤 퀴즈가 나올까요?</p>
+            </div>
+          </div>
+          <div className="card card-compact w-38 bg-base-100 shadow-xl">
+            <figure></figure>
+            <div className="card-body">
+              <h2 className="card-title">Q3.</h2>
+              <p>지구계 구성요소에는 생물권이 있다</p>
+            </div>
+          </div>
+        </div>
+
+        <div> hi </div>
+        <div className="w-40"></div>
+      </div>
+    </div>
+  );
+}
